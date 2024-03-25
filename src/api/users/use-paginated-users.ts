7b66ed0f25da7ef587df43078850d89c630b1ae7
제일 <joimh7@gmail.com>
2024-03-25 15:33:18 +0900

    페이지 기능에서 useQuery 2번 반복 했을 때 어떻게 되는지 테스트

--- src/api/users/use-paginated-users.ts
+++ src/api/users/use-paginated-users.ts
@@ -6,17 +6,36 @@ type Props = {
   pageLimit: number;
 };
 
+type Params = {
+  queryKey: [string, { status: string; page: number; }];
+}
+
+const status = 'active';
+
 export function usePaginatedUsers({ page, pageLimit }: Props) {
-  const getPaginatedUsersFn = async (p = page) => {
-    const response = await apiClient.get(`?_page=${p}&_limit=${pageLimit}`);
+  const getPaginatedUsersFn = async ({ queryKey }: Params) => {
+    const [, { status, page }] = queryKey;
+    const response = await apiClient.get(`?_page=${page}&_limit=${pageLimit}&status=${status}`);
     return response.data;
   };
 
-  return useQuery(
-    userQueryKeys.pagination(page),
-    () => getPaginatedUsersFn(page),
+  const params: Params = { queryKey: ['users', { status, page }] };
+
+  useQuery(
+    ['users', page, status],
+    () => getPaginatedUsersFn(params),
     {
       keepPreviousData: true,
+      staleTime: 1000 * 60 * 5, // 캐시 유지 시간: 5분
+    }
+  );
+
+  return useQuery(
+    ['users', status, page],
+    () => getPaginatedUsersFn(params),
+    {
+      keepPreviousData: true,
+      staleTime: 1000 * 60 * 5, // 캐시 유지 시간: 5분
     }
   );
 }
